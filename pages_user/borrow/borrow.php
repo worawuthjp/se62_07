ac6ba12a9fa7eaffa4fa0ef1588b01c1cf7b6b1f
worawuthjp <popcorns1udio@gmail.com>
2020-03-25 02:23:44 +0700

    UI UPDATE

--- pages_user/borrow/borrow.php
+++ pages_user/borrow/borrow.php
@@ -32,6 +32,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
+
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1>ยืมอุปกรณ์</h1>
